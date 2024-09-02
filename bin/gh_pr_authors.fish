4890c45cd165c3a86aaf52e47b24246689a3bf2f
Daniel D. Beck <daniel@ddbeck.com>
2024-09-02 15:59:03 +0200

    Format jq queries more cleanly

--- bin/gh_pr_authors.fish
+++ bin/gh_pr_authors.fish
@@ -2,9 +2,10 @@
 
 # TODO: figure out how to use `gh`'s go templates instead of TSV with `column`
 
-gh pr list --limit=100 --json author,title,url --jq 'group_by(.author.login) |
-  sort_by(.[].length) |
-  reverse |
-  .[] |
-  [. | length] + ["@" + .[0].author.login] + ["https://github.com/web-platform-dx/web-features/pulls/" + .[0].author.login] |
-  @tsv' | column -t -s=\t
+gh pr list --limit=100 --json author,title,url --jq '
+  group_by(.author.login)
+  | sort_by(.[].length)
+  | reverse
+  | .[]
+  | [. | length] + ["@" + .[0].author.login] + ["https://github.com/web-platform-dx/web-features/pulls/" + .[0].author.login]
+  |@tsv' | column -t -s=\t
--- bin/gh_prs_by_size.fish
+++ bin/gh_prs_by_size.fish
@@ -3,9 +3,9 @@
 # TODO: figure out how to use `gh`'s go templates instead of TSV with `column`
 
 gh pr list --limit=100 --json additions,deletions,author,title,url,isDraft,labels --jq '
-  map(. + { size: (.additions + .deletions) }) |
-  sort_by(.size) |
-  reverse |
-  .[] |
-  [.size, ("@" + .author.login), .title, (if .isDraft then "DRAFT " else " " end), .url]
+  map(. + { size: (.additions + .deletions) })
+  | sort_by(.size)
+  | reverse
+  | .[]
+  | [.size, ("@" + .author.login), .title, (if .isDraft then "DRAFT " else " " end), .url]
   | @tsv' | column -t -s=\t
