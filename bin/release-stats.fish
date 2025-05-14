64702082b431cc5e76ad42e49aa0db9c2069f08f
Daniel D. Beck <daniel@ddbeck.com>
2025-05-14 15:28:07 +0200

    Print default stats and option for choose

--- bin/release-stats.fish
+++ bin/release-stats.fish
@@ -1,23 +1,30 @@
 #!/usr/bin/env fish
 
-set -l release (begin
-  echo main
-  echo latest
-  git tag | rg '^v'
-end | fzf)
+argparse c/choose -- $argv
+set releases latest main
 
-if test $release = latest
-    set release (gh release view --json tagName --jq .tagName)
+if set -ql _flag_choose
+    # Provide v2.6.0 or greater, since there's no stats script before then
+    set --append releases (git tag | rg '^v(?:2\.(?:[6-9]|[1-9][0-9])|[3-9][0-9]*\.\d+)\.\d+')
+    set releases (string join0 $releases | fzf --read0 --preview 'cat {+f}' --bind 'tab:toggle+end-of-line+unix-line-discard' --multi)
 end
 
-if git checkout --quiet --detach $release
-    echo $release
-    if npm install --silent
-        echo ```
-        npx tsx ./scripts/stats.ts
-        echo ```
+for rel in $releases
+    set -l release $rel
+    if test $rel = latest
+        set release (gh release view --json tagName --jq .tagName)
     end
-    if git checkout --quiet -
-        npm install --silent
+
+    if git checkout --quiet --detach $release
+        echo $release
+        if npm install --silent
+            echo ```
+            npx tsx ./scripts/stats.ts
+            echo ```
+        end
+        if git checkout --quiet -
+            npm install --silent
+        end
     end
+    echo
 end
